49dbebd708f7415cbc7f7a0ec5485ca98bc1da50
Julieta You <nounou@nounoudemacbook-pro.local>
2016-01-08 00:13:45 +0800

    first app project, first commit

---
+++ wipartner/wipartner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wipartner
+//
+//  Created by nounou on 1/8/16.
+//  Copyright © 2016 abmjba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ wipartner/wipartner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  wipartner
+//
+//  Created by nounou on 1/8/16.
+//  Copyright © 2016 abmjba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
